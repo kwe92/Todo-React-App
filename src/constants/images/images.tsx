0136e7c8916745cac61a00ea714e0e61ef95633a
Kwe <Kweayon@gmail.com>
2023-06-11 22:23:14 -0500

    Inital Commit Images model

---
+++ src/constants/images/images.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import bgDesktopDark from "../../assets/images/bg-desktop-dark.jpg";
+import bgDesktopLight from "../../assets/images/bg-desktop-light.jpg";
+import bgMobileDark from "../../assets/images/bg-mobile-dark.jpg";
+import bgMobileLight from "../../assets/images/bg-mobile-light.jpg";
+import iconCheck from "../../assets/images/icon-check.svg";
+import iconCross from "../../assets/images/icon-cross.svg";
+import iconMoon from "../../assets/images/icon-moon.svg";
+import iconSun from "../../assets/images/icon-sun.svg";
+
+export default {
+  bgDesktopDark,
+  bgDesktopLight,
+  bgMobileDark,
+  bgMobileLight,
+  iconCheck,
+  iconCross,
+  iconMoon,
+  iconSun,
+};
