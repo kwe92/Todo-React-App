cc7c9c26e25a57de81c0bec24f1f1a8e81dd5505
Kwe <Kweayon@gmail.com>
2023-06-13 13:51:44 -0500

    input todo

---
+++ src/components/create_todo_input/CreateTodo.tsx
@@ -0,0 +1,16 @@
+import { Wrapper } from "./CreateTodoStyles";
+import { StyledInput } from "./CreateTodoStyles";
+import { StyledInputContainer } from "./CreateTodoStyles";
+export default function CreateTodo() {
+  return (
+    <Wrapper>
+      <StyledInputContainer>
+        <StyledInput
+          placeholder="Create a new todo..."
+          onChange={() => {}}
+          value={""}
+        ></StyledInput>
+      </StyledInputContainer>
+    </Wrapper>
+  );
+}
