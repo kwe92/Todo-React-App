4ce0474d3bcd9f11cbda334e667cc3792366b438
Kwe <Kweayon@gmail.com>
2023-06-13 13:52:55 -0500

    BackgroundImageStyles

--- src/components/background_image/BackgroundImageStyles.tsx
+++ src/components/background_image/BackgroundImageStyles.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { Row } from "../../styles/layout/Flex";
 
-interface Props {
+const AppBackgroundImage = styled(Row)<{
   url: string;
-}
-
-const AppBackgroundImage = styled(Row)<Props>`
+}>`
   position: absolute;
   width: 100vw;
   height: 18.75rem;
