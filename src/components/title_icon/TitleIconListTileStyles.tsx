acd93731310c40e2726579a63bcf8e4dff5089a8
Kwe <Kweayon@gmail.com>
2023-06-13 13:50:13 -0500

    Title Icon Styles

---
+++ src/components/title_icon/TitleIconListTileStyles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Row } from "../../styles/layout/Flex";
+
+const TitleIcon = styled(Row)`
+  justify-content: space-between;
+  align-items: start;
+  width: 100%;
+`;
+
+const Title = styled(Row)`
+  align-items: start;
+  font-size: 40px;
+  color: white;
+`;
+
+export { TitleIcon, Title };
