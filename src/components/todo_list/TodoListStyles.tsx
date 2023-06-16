f3c5f045cb2cccfb55ce6ff382914cdd66b99f23
Kwe <Kweayon@gmail.com>
2023-06-16 17:50:07 -0500

    added box shadows

--- src/components/todo_list/TodoListStyles.tsx
+++ src/components/todo_list/TodoListStyles.tsx
@@ -7,7 +7,6 @@ const TodoListContainer = styled.ul`
   padding-bottom: 50px;
   margin-top: 1.5rem;
   margin-bottom: 2.5rem;
-
   border-radius: 2rem;
   background: white;
   box-shadow: 0 35px 50px -15px rgba(194, 195, 214, 0.5);
