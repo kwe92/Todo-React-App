831fd95c3fbb563b17b46ec7f7ed3f6b65d6509c
Kwe <Kweayon@gmail.com>
2023-06-16 16:57:44 -0500

    TodoListStyles

--- src/components/todo_list/TodoListStyles.tsx
+++ src/components/todo_list/TodoListStyles.tsx
@@ -3,8 +3,6 @@ import styled from "styled-components";
 const TodoListContainer = styled.ul`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   width: 100%;
   height: 440px;
   margin-top: 1.5rem;
