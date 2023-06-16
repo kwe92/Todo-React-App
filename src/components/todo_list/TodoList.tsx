8b1bba1f0cfa2fa19dcaa66f8bc5f9c178a01de8
Kwe <Kweayon@gmail.com>
2023-06-16 17:29:13 -0500

    added temp date to display multiple todos

--- src/components/todo_list/TodoList.tsx
+++ src/components/todo_list/TodoList.tsx
@@ -3,9 +3,16 @@ import { TodoListContainer } from "./TodoListStyles";
 import ListItem from "../list_item/ListItem";
 
 export default function TodoList() {
-  return (
-    <TodoListContainer>
-      <ListItem todo="Aim single mindedly at one thing and see what happens" />
-    </TodoListContainer>
-  );
+  const qoutes = [
+    "Aim single mindedly at one thing and see what happens",
+    "Begin to begin is half the work let half still remain again begin this and though wilt have finished",
+    "Do not respect the judgement of any man, but hear any man regardless of his station, but do not fear any man because the judment of man is GODS",
+    "Read in the morning.",
+  ];
+
+  const listItems = qoutes.map((quote, index) => {
+    return <ListItem key={quote.substring(0, 15) + index} todo={quote} />;
+  });
+
+  return <TodoListContainer>{listItems}</TodoListContainer>;
 }
