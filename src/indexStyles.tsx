b3240eb6da1cb6740a61453bf6f3468513d3fb2b
Kwe <Kweayon@gmail.com>
2023-06-15 21:54:57 -0500

    Implemented createGlobalStyle

---
+++ src/indexStyles.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const AppGlobalStyle = createGlobalStyle`
+body{
+    margin: 0;
+    padding: 0;
+    font-family: 'Helvetica', 'Arial', sans-serif;
+}
+
+ul {
+    margin: 0;
+    padding: 0;
+}
+
+li {
+    list-style: none;
+}
+
+`;
