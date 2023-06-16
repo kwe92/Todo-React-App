3c0f94fc8cbdeec3c436ffb15ed68d1fec578bb6
Kwe <Kweayon@gmail.com>
2023-06-15 20:25:47 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { AppBackgroundImage } from "./components/background_image/BackgroundImageStyles";
 import { images } from "./constants/images";
 import styled from "styled-components";
 import { Column } from "./styles/layout/Flex";
 import TitleIconListTile from "./components/title_icon/TitleIconListTile";
 import CreateTodo from "./components/create_todo_input/CreateTodo";
+import BackgroundImage from "./components/app_background_image/BackGroundImage";
+import { MainContainer, TodoSection } from "./AppStyles";
 export default function App() {
   return (
     <>
-      <img
-        src={images.bgDesktopLight}
-        style={{ height: "18.75rem", position: "absolute", width: "100%" }}
-      />
+      <BackgroundImage src={images.bgDesktopLight} />
       <MainContainer>
         <TodoSection>
           <TitleIconListTile />
@@ -21,19 +19,3 @@ export default function App() {
     </>
   );
 }
-const MainContainer = styled(Column)`
-  width: 100vw;
-  height: 100vh;
-  justify-content: start;
-`;
-
-const TodoSection = styled(Column)`
-  position: relative;
-  padding-top: 70px;
-  align-items: start;
-  justify-content: start;
-  width: 540px;
-  height: 740px;
-  z-index: 1;
-  // background: #ff9999;
-`;
