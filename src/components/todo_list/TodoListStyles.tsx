2863f5b54aa6db2ccf44925a5614d757a805a2b1
Kwe <Kweayon@gmail.com>
2023-06-21 20:23:01 -0500

    Added TextButton, ButtonContainer, FilterSection, ItemsLeftSection, BottomSection

--- src/components/todo_list/TodoListStyles.tsx
+++ src/components/todo_list/TodoListStyles.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
-// TODO: Add box shadows
+import { Row } from "../../styles/layout/Flex";
 const TodoListContainer = styled.ul`
   display: flex;
   flex-direction: column;
   width: 100%;
-  padding-bottom: 50px;
   margin-top: 1.5rem;
   margin-bottom: 2.5rem;
   border-radius: 2rem;
@@ -12,4 +11,40 @@ const TodoListContainer = styled.ul`
   box-shadow: 0 35px 50px -15px rgba(194, 195, 214, 0.5);
 `;
 
-export { TodoListContainer };
+const BottomSection = styled(Row)`
+  width: 100%;
+  height: 3.125rem;
+  justify-content: space-between;
+`;
+
+const ItemsLeftSection = styled(Row)`
+  padding-left: 1.5rem;
+`;
+
+const FilterSection = styled(Row)``;
+
+const TextButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+  &:active {
+    color: orange;
+  }
+  &:hover {
+    color: rgba(117, 160, 248, 1);
+  }
+`;
+
+const ButtonContainer = styled(Row)`
+  width: 10.3125rem;
+  justify-content: space-evenly;
+`;
+
+export {
+  TodoListContainer,
+  BottomSection,
+  ItemsLeftSection,
+  FilterSection,
+  TextButton,
+  ButtonContainer,
+};
